3ff5e9fc45504899ede6925a9faae7656a413307
Anshu kumar <40786793+aanshu-kumar@users.noreply.github.com>
2024-12-20 21:22:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
-
+	let f_name=document.getElementById("f_name").value
+	let l_name=document.getElementById("l_name").value
+	alert(f_name+" "+l_name)
+	
 }
